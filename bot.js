d4b6915d5ea6cf762d04b68dbdf4d13b18e9643e
avatar99 <51578188+avatar99@users.noreply.github.com>
2019-09-07 18:52:33 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -22,7 +22,24 @@ client2.on('ready', () => {
    console.log(`----------------`);
 });
 
-
+client.on('message', message => {
+    if (message.author.bot) return;
+    if (message.content.startsWith("-say")) {
+if(!message.member.hasPermission('ADMINISTRATOR')) return      message.channel.send('**You dont have** `ADMINISTRATOR` **permission**');
+var args = message.content.trim().split(/ +/g).slice(1);
+let cname = args[0];
+let chan = message.guild.channels.find(element => element.name === cname);
+if (chan) {
+    let text = args.slice(1).join(" ");
+    message.delete();
+    chan.send(text);
+} else {
+    let text = args.join(" ");
+    message.delete();
+    message.channel.send(text);
+   }
+}
+});
 
 client.on('message', message => {
     if(message.content === prefix+'راتب'){
